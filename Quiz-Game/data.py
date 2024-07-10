c480930d35f7731f0c34b35958a940ffe28e21a0
Anish Shukla <138965632+shuklaji7058@users.noreply.github.com>
2024-07-10 13:27:40 +0530

    Add files via upload

---
+++ Quiz-Game/data.py
@@ -0,0 +1,23 @@
+question_data = [
+    {"text": "In Python, the `print()` function is used to output information to the console.", "answer": "True"},
+    {"text": "The Indian Premier League (IPL) was first held in 2018.", "answer": "False"},
+    {"text": "In Python, lists are mutable, meaning they can be changed after their creation.", "answer": "True"},
+    {"text": "Rohit Sharma scored the first double century in ODI cricket.", "answer": "False"},
+    {"text": "The `len()` function in Python returns the number of items in a list, string, or dictionary.", "answer": "True"},
+    {"text": "Virat Kohli is the first Indian cricketer to score a century in the Indian Premier League (IPL).", "answer": "False"},
+    {"text": "In Python, dictionaries store data in key-value pairs and the keys must be unique.", "answer": "True"},
+    {"text": "Mahendra Singh Dhoni is known for his helicopter shot.", "answer": "True"},
+    {"text": "In Python, a tuple is similar to a list, but it is immutable.", "answer": "True"},
+    {"text": "Anil Kumble is the only Indian bowler to have taken all 10 wickets in a single Test inning.", "answer": "True"},
+    {"text": "In Python, the `range()` function generates a list of numbers.", "answer": "False"},
+    {"text": "Rohit Sharma has the highest individual score in ODI cricket.", "answer": "True"},
+    {"text": "The `input()` function in Python is used to get user input.", "answer": "True"},
+    {"text": "India won its first Cricket World Cup in 1983.", "answer": "True"},
+    {"text": "In Python, `None` is a data type.", "answer": "True"},
+    {"text": "The Duckworth-Lewis method is used in cricket to calculate target scores in interrupted matches.", "answer": "True"},
+    {"text": "In Python, you can create a virtual environment using the `venv` module.", "answer": "True"},
+    {"text": "Kapil Dev was the captain of the Pakistan cricket team during the 1983 World Cup victory.", "answer": "False"},
+    {"text": "In Python, a `set` is an ordered collection of unique elements.", "answer": "False"},
+    {"text": "Harbhajan Singh is known as the 'Turbanator'.", "answer": "True"}
+]
+
